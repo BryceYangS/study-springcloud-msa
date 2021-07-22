0eb058851248acee7b1492285844f069131262e0
Bryce Yang <nash31@korea.ac.kr>
2021-07-23 01:24:50 +0900

    spring-cloud-gateway/eureka

--- first-service/src/main/java/com/example/firstservice/FirstServiceController.java
+++ first-service/src/main/java/com/example/firstservice/FirstServiceController.java
@@ -1,5 +1,8 @@
 package com.example.firstservice;
 
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,6 +14,12 @@ import lombok.extern.slf4j.Slf4j;
 @RestController
 @RequestMapping("/first-service")
 public class FirstServiceController {
+
+	Environment env;
+
+	public FirstServiceController(Environment env) {
+		this.env = env;
+	}
 
 	@GetMapping("/welcome")
 	public String welcome() {
@@ -24,7 +33,10 @@ public class FirstServiceController {
 	}
 
 	@GetMapping("/check")
-	public String check() {
-		return "Hi, there. There is a message from First Service";
+	public String check(HttpServletRequest request) {
+
+		log.info("Server port={}", request.getServerPort());
+
+		return String.format("Hi, there. There is a message from First Service on Port %s", env.getProperty("local.server.port"));
 	}
 }
